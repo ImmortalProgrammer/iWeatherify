f1e70fd73c1bd59736c16f8bd1e257b71aab36f4
Shlomo Cohen <shlomcoo@buffalo.edu>
2023-02-22 03:07:53 -0500

    Generated HTML/CSS code based off Figma Homepage Design

---
+++ index.php
@@ -0,0 +1,259 @@
+	<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=1440, maximum-scale=1.0" />
+    <link rel="shortcut icon" type="image/png" href="https://animaproject.s3.amazonaws.com/home/favicon.png" />
+    <link rel="stylesheet" type="text/css" href="css/website-home-page-not-logged-in.css" />
+    <link rel="stylesheet" type="text/css" href="css/styleguide.css" />
+    <link rel="stylesheet" type="text/css" href="css/globals.css" />
+  </head>
+  <body style="margin: 0; background: #ffffff">
+    <input type="hidden" id="anPageName" name="page" value="website-home-page-not-logged-in" />
+    <div class="container-center-horizontal">
+      <div class="website-home-page-not-logged-in screen">
+        <div class="overlap-group3">
+          <div class="outfit-of-the-day ui---16-regular">
+            <div class="outfit-of-the-day-1">Outfit of the day!</div>
+            <div class="overlap-group-1">
+              <div class="rectangle-242"></div>
+              <img class="line-81 line" src="img/line-81-2.svg" alt="Line 81" />
+              <img class="line-82 line" src="img/line-82-2.svg" alt="Line 82" />
+            </div>
+            <div class="consider-wearing">
+              <span class="ui---16-regular"
+                >Consider wearing...<br />T-shirt<br />Sweater<br />Jeans<br />Sneakers</span
+              >
+            </div>
+            <div class="save">Save</div>
+          </div>
+          <div class="group-3 group">
+            <div class="flex-col flex">
+              <div class="input-search"><div class="search ui---16-medium">Search city or zip</div></div>
+              <div class="flex-row flex">
+                <div class="weather">
+                  <div class="place ui---30-regular">New York</div>
+                  <div class="overlap-group-2">
+                    <div class="text-1 inter-normal-black-96px">45°</div>
+                    <p class="h-50-l40 ui---16-regular">H: 50 L:40<br />Feels like: 40</p>
+                  </div>
+                </div>
+                <div class="rectangle-263"></div>
+              </div>
+              <div class="weather-for-today">
+                <article class="weather-throughout-the-day">
+                  <div class="now">
+                    <div class="now-1 inter-medium-black-16px">Now</div>
+                    <img
+                      class="fluentweather-rain-s"
+                      src="img/fluent-weather-rain-showers-day-24-regular-18.svg"
+                      alt="fluent:weather-rain-showers-day-24-regular"
+                    />
+                    <div class="group-2 group">
+                      <div class="overlap-group2">
+                        <div class="overlap-group1">
+                          <div class="number inter-normal-black-12px">20</div>
+                          <div class="degree"></div>
+                        </div>
+                        <div class="text inter-normal-black-12px">/</div>
+                      </div>
+                      <div class="overlap-group">
+                        <div class="number inter-normal-black-12px">24</div>
+                        <div class="degree"></div>
+                      </div>
+                    </div>
+                    <div class="x74-rain inter-normal-black-12px">74% rain</div>
+                  </div>
+                  <div class="now-2">
+                    <div class="now-3 inter-medium-black-16px">10:00</div>
+                    <img
+                      class="fluentweather-rain-s-1"
+                      src="img/fluent-weather-rain-showers-day-24-regular-18.svg"
+                      alt="fluent:weather-rain-showers-day-24-regular"
+                    />
+                    <div class="group-2-1">
+                      <div class="overlap-group2">
+                        <div class="overlap-group1">
+                          <div class="number inter-normal-black-12px">20</div>
+                          <div class="degree"></div>
+                        </div>
+                        <div class="text inter-normal-black-12px">/</div>
+                      </div>
+                      <div class="overlap-group">
+                        <div class="number inter-normal-black-12px">24</div>
+                        <div class="degree"></div>
+                      </div>
+                    </div>
+                    <div class="x74-rain-1 inter-normal-black-12px">74% rain</div>
+                  </div>
+                  <div class="now-4">
+                    <div class="now-5 inter-medium-black-16px">11:00</div>
+                    <img
+                      class="fluentweather-rain-s"
+                      src="img/fluent-weather-rain-showers-day-24-regular-18.svg"
+                      alt="fluent:weather-rain-showers-day-24-regular"
+                    />
+                    <div class="group-2 group">
+                      <div class="overlap-group2">
+                        <div class="overlap-group1">
+                          <div class="number inter-normal-black-12px">20</div>
+                          <div class="degree"></div>
+                        </div>
+                        <div class="text inter-normal-black-12px">/</div>
+                      </div>
+                      <div class="overlap-group">
+                        <div class="number inter-normal-black-12px">24</div>
+                        <div class="degree"></div>
+                      </div>
+                    </div>
+                    <div class="x74-rain inter-normal-black-12px">74% rain</div>
+                  </div>
+                </article>
+                <article class="weather-throughout-the-day">
+                  <div class="now-6">
+                    <div class="now-7 inter-medium-black-16px">12:00</div>
+                    <img
+                      class="fluentweather-rain-s"
+                      src="img/fluent-weather-rain-showers-day-24-regular-18.svg"
+                      alt="fluent:weather-rain-showers-day-24-regular"
+                    />
+                    <div class="group-2 group">
+                      <div class="overlap-group2">
+                        <div class="overlap-group1">
+                          <div class="number inter-normal-black-12px">20</div>
+                          <div class="degree"></div>
+                        </div>
+                        <div class="text inter-normal-black-12px">/</div>
+                      </div>
+                      <div class="overlap-group">
+                        <div class="number inter-normal-black-12px">24</div>
+                        <div class="degree"></div>
+                      </div>
+                    </div>
+                    <div class="x74-rain inter-normal-black-12px">74% rain</div>
+                  </div>
+                  <div class="now-8">
+                    <div class="now-9 inter-medium-black-16px">13:00</div>
+                    <img
+                      class="fluentweather-rain-s-1"
+                      src="img/fluent-weather-rain-showers-day-24-regular-18.svg"
+                      alt="fluent:weather-rain-showers-day-24-regular"
+                    />
+                    <div class="group-2-1">
+                      <div class="overlap-group2">
+                        <div class="overlap-group1">
+                          <div class="number inter-normal-black-12px">20</div>
+                          <div class="degree"></div>
+                        </div>
+                        <div class="text inter-normal-black-12px">/</div>
+                      </div>
+                      <div class="overlap-group">
+                        <div class="number inter-normal-black-12px">24</div>
+                        <div class="degree"></div>
+                      </div>
+                    </div>
+                    <div class="x74-rain-1 inter-normal-black-12px">74% rain</div>
+                  </div>
+                  <div class="now-10">
+                    <div class="now-11 inter-medium-black-16px">14:00</div>
+                    <img
+                      class="fluentweather-rain-s-1"
+                      src="img/fluent-weather-rain-showers-day-24-regular-23.svg"
+                      alt="fluent:weather-rain-showers-day-24-regular"
+                    />
+                    <div class="group-2-1">
+                      <div class="overlap-group2">
+                        <div class="overlap-group1">
+                          <div class="number inter-normal-black-12px">20</div>
+                          <div class="degree"></div>
+                        </div>
+                        <div class="text inter-normal-black-12px">/</div>
+                      </div>
+                      <div class="overlap-group">
+                        <div class="number inter-normal-black-12px">24</div>
+                        <div class="degree"></div>
+                      </div>
+                    </div>
+                    <div class="x74-rain-1 inter-normal-black-12px">74% rain</div>
+                  </div>
+                </article>
+                <article class="weather-throughout-the-day">
+                  <div class="now-12">
+                    <div class="now-13 inter-medium-black-16px">15:00</div>
+                    <img
+                      class="fluentweather-rain-s"
+                      src="img/fluent-weather-rain-showers-day-24-regular-23.svg"
+                      alt="fluent:weather-rain-showers-day-24-regular"
+                    />
+                    <div class="group-2 group">
+                      <div class="overlap-group2">
+                        <div class="overlap-group1">
+                          <div class="number inter-normal-black-12px">20</div>
+                          <div class="degree"></div>
+                        </div>
+                        <div class="text inter-normal-black-12px">/</div>
+                      </div>
+                      <div class="overlap-group">
+                        <div class="number inter-normal-black-12px">24</div>
+                        <div class="degree"></div>
+                      </div>
+                    </div>
+                    <div class="x74-rain inter-normal-black-12px">74% rain</div>
+                  </div>
+                  <div class="now-14">
+                    <div class="now-15 inter-medium-black-16px">16:00</div>
+                    <img
+                      class="fluentweather-rain-s"
+                      src="img/fluent-weather-rain-showers-day-24-regular-18.svg"
+                      alt="fluent:weather-rain-showers-day-24-regular"
+                    />
+                    <div class="group-2 group">
+                      <div class="overlap-group2">
+                        <div class="overlap-group1">
+                          <div class="number inter-normal-black-12px">20</div>
+                          <div class="degree"></div>
+                        </div>
+                        <div class="text inter-normal-black-12px">/</div>
+                      </div>
+                      <div class="overlap-group">
+                        <div class="number inter-normal-black-12px">24</div>
+                        <div class="degree"></div>
+                      </div>
+                    </div>
+                    <div class="x74-rain inter-normal-black-12px">74% rain</div>
+                  </div>
+                  <div class="now-16">
+                    <div class="now-17 inter-medium-black-16px">17:00</div>
+                    <img
+                      class="fluentweather-rain-s"
+                      src="img/fluent-weather-rain-showers-day-24-regular-18.svg"
+                      alt="fluent:weather-rain-showers-day-24-regular"
+                    />
+                    <div class="group-2 group">
+                      <div class="overlap-group2">
+                        <div class="overlap-group1">
+                          <div class="number inter-normal-black-12px">20</div>
+                          <div class="degree"></div>
+                        </div>
+                        <div class="text inter-normal-black-12px">/</div>
+                      </div>
+                      <div class="overlap-group">
+                        <div class="number inter-normal-black-12px">24</div>
+                        <div class="degree"></div>
+                      </div>
+                    </div>
+                    <div class="x74-rain inter-normal-black-12px">74% rain</div>
+                  </div>
+                </article>
+              </div>
+            </div>
+            <div class="ellipse-6"></div>
+            <img class="vector" src="img/vector-2.svg" alt="Vector" />
+          </div>
+          <h1 class="login-to-view-a-pers">Login to view a personalized outfit of the day!</h1>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
+
