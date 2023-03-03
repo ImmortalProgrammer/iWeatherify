e3ea4c9c5603845df16298f949de6433a52baab9
shlc001 <98677355+shlc001@users.noreply.github.com>
2023-03-02 19:15:02 -0500

    Converted into React Code #36

---
+++ components/Ellipse6/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Ellipse6.css";
+
+class Ellipse6 extends React.Component {
+  render() {
+    return <div className="ellipse-6"></div>;
+  }
+}
+
+export default Ellipse6;
