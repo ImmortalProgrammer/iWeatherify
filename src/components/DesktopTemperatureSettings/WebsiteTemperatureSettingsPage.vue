0f0858fd8e94e4f4bf762a0d85341ddd6296bf65
JayXPan <jpan26@buffalo.edu>
2023-03-05 16:37:33 -0500

    Temperature Setting Page done for desktop view #40

--- src/components/DesktopTemperatureSettings/WebsiteTemperatureSettingsPage.vue
+++ src/components/DesktopTemperatureSettings/WebsiteTemperatureSettingsPage.vue
@@ -2,29 +2,44 @@
   <div class="container-center-horizontal">
     <div class="website-temperature-settings-page screen">
       <h1 class="title-3">{{ title }}</h1>
-      <img class="default-logo-3" :src="defaultLogo3" alt="Default Logo 3" />
       <div>
+          <img class="default-logo-3" :src="defaultLogo3" alt="Default Logo 3" />
           <img class="profile-img" :src="ellipse6Props.src">
-      </div>
-      <div>
-      <img
-      class="vector-3"
-      src="https://anima-uploads.s3.amazonaws.com/projects/6402851d6a37db7167320ed4/releases/64028608c9953e08464983be/img/vector-1.svg"
-      alt="Vector"
-      />
+          <img
+          class="vector-3"
+          src="https://anima-uploads.s3.amazonaws.com/projects/6402851d6a37db7167320ed4/releases/64028608c9953e08464983be/img/vector-1.svg"
+          alt="Vector" 
+          />
       </div>
       <div>
         <div class="entire-temp-scale">
           <div class="green-scale"></div>
-            <div class="temperatures">
-            100 75 50 25 0
-            </div>
+          <div class="temp-rows">
+            <div class="temperatures-font">100</div>
           </div>
-          <div class="hot-warm-just-right-1">
-          Hot Warm Just right Chilly Cold Freezing
+          <div class="temp-rows">
+            <div class="temperatures-font">75</div>
+          </div>
+          <div class="temp-rows">
+            <div class="temperatures-font">50</div>
+          </div>
+          <div class="temp-rows">
+            <div class="temperatures-font">25</div>
+          </div>
+          <div class="temp-rows">
+            <div class="temperatures-font">0</div>
           </div>
         </div>
+        <div class="hot-to-freezing-container">
+            <div class="hot-to-freezing-font">Hot</div>
+            <div class="hot-to-freezing-font">Warm</div>
+            <div class="hot-to-freezing-font">Just Right</div>
+            <div class="hot-to-freezing-font">Chilly</div>
+            <div class="hot-to-freezing-font">Cold</div>
+            <div class="hot-to-freezing-font">Freezing</div>
+        </div>
       </div>
+    </div>
   </div>
 </template>
 
@@ -74,75 +89,87 @@ export default {
   position: absolute;
   background-image: url(https://anima-uploads.s3.amazonaws.com/projects/6402851d6a37db7167320ed4/releases/640286bb66ed049392a82543/img/background.svg);
   background-size: 100% 100%;
-  width: 5.9722vw;
-  height: 68.3594vh;
-  left: 44.0278vw;
-  top: 17.0898vh;
+  width: 6vw;
+  height: 55vh;
+  left: 45vw;
+  top: 17vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
 }
 
 .green-scale {
   position: absolute;
   background-image: url(https://anima-uploads.s3.amazonaws.com/projects/6402851d6a37db7167320ed4/releases/640286bb66ed049392a82543/img/background-1.svg);
   background-size: 100% 100%;
-  width: 5.9722vw;
-  height: 42.2852vh;
-  top: 26vh;
+  width: 6vw;
+  height: 30vh;
+  top: 25vh;
 }
 
-.temperatures {
+.temp-rows {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 10%;
+}
+
+.temperatures-font {
   position: absolute;
   color: var(--black);
-  width: 5.7639vw;
-  height: 77.7344vh;
-  top: 17.9648vh;
   font-family: 'Inter';
   font-style: normal;
   font-weight: 600;
   font-size: 1.5625vw;
-  line-height: 1.875vw;
+}
+
+.hot-to-freezing-container {
+  position: absolute;
+  width: 9vw;
+  height: 55vh;
+  left: 51vw;
+  top: 17vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  line-height: 2.5vw;
   text-align: center;
 }
 
-.hot-warm-just-right-1 {
-  position: absolute;
+.hot-to-freezing-font {
   color: var(--black);
-  width: 9.7222vw;
-  height: 74.0234vh;
-  left: 51.1111vw;
-  top: 18.75vh;
-  font-weight: 400;
-  line-height: 2.5vw;
-  text-align: center;
   font-family: 'Inter';
   font-style: normal;
   font-weight: 400;
-  font-size: 2.0833vw; 
-  line-height: 2.5vw;
+  font-size: 1.5vw;
 }
 
 .vector-3 {
   position: absolute;
-  width: 2.8472vw;
-  height: 2.8472vw;
-  left: 93.75vw;
-  top: 5.1758vh;
+  width: 4vw;
+  height: auto;
+  left: 93vw;
+  top: 3vh;
+  object-fit: cover;
 }
 
 .default-logo-3 {
   position: absolute;
-  width: 5vw;
-  height: 5vh;
+  max-width: 100%;
+  width: 10vw;
+  height: auto;
   left: 5.8333vw;
-  top: 2.6367vh;
+  top: 3vh;
   object-fit: cover;
 }
 
 .profile-img {
   position: absolute;
-  width: 3.4722vw; 
-  height: 3.4722vw; 
-  left: 87.9167vw; 
-  top: 4.6875vh;
+  width: 6vw;
+  height: auto;
+  left: 87vw; 
+  top: 3vh;
   object-fit: cover;
 }
 </style>
