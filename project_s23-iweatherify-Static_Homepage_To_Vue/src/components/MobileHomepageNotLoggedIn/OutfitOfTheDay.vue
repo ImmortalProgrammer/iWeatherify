22b07f76ec7a93824179d2184cc3dcee1a01687f
Cheri342 <71862946+Cheri342@users.noreply.github.com>
2023-03-05 01:43:32 -0500

    Add files via upload
    Edit Location Settings Page for desktop #43

---
+++ project_s23-iweatherify-Static_Homepage_To_Vue/src/components/MobileHomepageNotLoggedIn/OutfitOfTheDay.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="outfit-of-the-day ui---16-regular">
+    <p class="outfit-of-the-day-1">{{ outfitOfTheDay }}</p>
+    <div class="overlap-group2-2">
+      <div class="rectangle-242"></div>
+      <img
+        class="line-81"
+        src="https://anima-uploads.s3.amazonaws.com/projects/6402851d6a37db7167320ed4/releases/640285252652d48db0de9030/img/line-81.svg"
+        alt="Line 81"
+      />
+      <img
+        class="line-82"
+        src="https://anima-uploads.s3.amazonaws.com/projects/6402851d6a37db7167320ed4/releases/640285252652d48db0de9030/img/line-82.svg"
+        alt="Line 82"
+      />
+    </div>
+    <p class="consider-wearing">
+      <span class="ui---16-regular">{{ spanText }}</span>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "OutfitOfTheDay",
+  props: ["outfitOfTheDay", "spanText"],
+};
+</script>
+
+<style>
+.outfit-of-the-day {
+  align-items: flex-start;
+  display: flex;
+  flex-direction: column;
+  margin-left: 3px;
+  margin-top: 35px;
+  min-height: 401px;
+  width: 326px;
+}
+
+.outfit-of-the-day-1 {
+  color: var(--black);
+  font-weight: 400;
+  line-height: normal;
+  margin-left: 2px;
+  margin-top: -1px;
+  min-height: 24px;
+  width: 324px;
+}
+
+.overlap-group2-2 {
+  height: 243px;
+  margin-top: 1px;
+  position: relative;
+  width: 323px;
+}
+
+.rectangle-242 {
+  background-color: var(--alto);
+  height: 242px;
+  left: 0;
+  position: absolute;
+  top: 0;
+  width: 322px;
+}
+
+.line-81 {
+  height: 241px;
+  left: 0;
+  object-fit: cover;
+  position: absolute;
+  top: 1px;
+  width: 323px;
+}
+
+.line-82 {
+  height: 243px;
+  left: 0;
+  object-fit: cover;
+  position: absolute;
+  top: 0;
+  width: 322px;
+}
+
+.consider-wearing {
+  color: var(--black);
+  font-weight: 400;
+  line-height: normal;
+  margin-left: 1px;
+  margin-top: 9px;
+  min-height: 95px;
+  width: 320px;
+}
+</style>
---
+++ project_s23-iweatherify-Static_Homepage_To_Vue/src/components/MobileUnits/MobileUnits.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="container-center-horizontal">
+    <div class="mobile-units screen">
+      <div class="overlap-group2">
+        <div class="overlap-group1">
+          <ios-status-bar-black :leftSide="iosStatusBarBlackProps.leftSide" :battery="iosStatusBarBlackProps.battery" />
+          <h1 class="title inter-bold-black-30px">{{ title }}</h1>
+        </div>
+        <img
+          class="vector"
+          src="https://anima-uploads.s3.amazonaws.com/projects/6402851d6a37db7167320ed4/releases/640285252652d48db0de9030/img/vector.svg"
+          alt="Vector"
+        />
+      </div>
+      <table-row />
+      <table-row2 />
+      <table-row3 />
+      <table-row4 />
+    </div>
+  </div>
+</template>
+
+<script>
+import IosStatusBarBlack from "./IosStatusBarBlack";
+import TableRow from "./TableRow";
+import TableRow2 from "./TableRow2";
+import TableRow3 from "./TableRow3";
+import TableRow4 from "./TableRow4";
+export default {
+  name: "MobileUnits",
+  components: {
+    IosStatusBarBlack,
+    TableRow,
+    TableRow2,
+    TableRow3,
+    TableRow4,
+  },
+  props: ["title", "iosStatusBarBlackProps"],
+};
+</script>
+
+<style>
+.mobile-units {
+  align-items: center;
+  background-color: var(--white);
+  border: 1px none;
+  display: flex;
+  flex-direction: column;
+  height: 812px;
+  position: relative;
+  width: 375px;
+}
+
+.overlap-group2 {
+  height: 79px;
+  position: relative;
+  width: 375px;
+}
+
+.overlap-group1 {
+  height: 79px;
+  left: 0;
+  position: absolute;
+  top: 0;
+  width: 375px;
+}
+
+.title {
+  left: 75px;
+  letter-spacing: 0;
+  line-height: normal;
+  position: absolute;
+  text-align: center;
+  top: 43px;
+  width: 227px;
+}
+
+.vector {
+  height: 22px;
+  left: 33px;
+  position: absolute;
+  top: 51px;
+  width: 22px;
+}
+</style>
