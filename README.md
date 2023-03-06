944fda289529c83d39ac38ff5b3d6199db226583
JayXPan <jpan26@buffalo.edu>
2023-03-05 19:02:09 -0500

    Units Page Done For Desktop #41

--- README.md
+++ README.md
@@ -1,1 +1,86 @@
-# project_s23-iweatherify
+# Directory
+
+### Fonts Directory
+
+* Holds all the different fonts that the website uses
+
+### Img Directory
+
+* Holds all the static images related to the project
+
+### Public Directory
+
+* This holds index.html, which is self-evident in its function (main html root)
+
+### Src Directory
+
+* The src directory holds the bulk of the code for the project
+
+
+* The components folder in the src directory holds the various folders pertaining to webpages and inside those subfolders holds the vue files (html, css, javascript) for that particular page. 
+
+
+* App.vue is the root of the program, think of it as a main method, but for vue
+
+
+* data.js holds all the javascript functionality and one needs to integrate with the components in .vue with the proper identifiers 
+
+
+* main.js gets the app running
+
+
+* router.js represents every page on the site and how to reach that site, follow the general format of what's in the file and you should be all good!
+
+### Rest of the files
+
+* Don't touch babel.config.js unless necessary
+
+
+* globals.css is a css file that applies to the entire website
+
+
+* package.json allows for collaboration as a build tool, lists dependency versions (don't touch unless necessary)
+
+
+* package-lock.json package-lock.json is "written to when a numerical value in a property such as the "version" property, or a dependency property is changed in package.json."
+
+
+* README.md is the documentation for this github
+
+
+* styleguide.css holds the various fonts of the website
+
+* vue.config.js is a config file that allows the website to run property
+
+# Instructions
+
+* Ensure that your repository on node version 12.22.12 and npm version 6.14.6 before building your project:
+
+### To install npm version 6.14.6 on your repository:
+* npm i npm@6.14.6
+
+### To install node version 12.22.12 on your repository:
+* npm install node@12.22.12
+
+## Localhost
+
+```
+cd package_code
+npm install
+npm start
+```
+
+Open [http://localhost:8080](http://localhost:8080).
+
+## Build for deploying
+
+Note: 
+* After building, you can the upload `dist` folder to cheshire using scp.
+
+* If you're trying this on cheshire, make sure you move the files out of the dist folder one directory up, so ../ otherwise it will not work!
+
+```
+cd dist
+npm install
+npm run build
+```
---
+++ src/components/DesktopUnits/WebsiteUnitsPage.vue
@@ -0,0 +1,114 @@
+<template>
+  <div class="container-center-horizontal">
+    <div class="website-units-page screen">
+      <div>
+        <img class="default-logo-4" :src="defaultLogo4" alt="Default Logo 4" />
+        <img img class="profile-img" :src="ellipse6Props.src" />
+        <img
+          class="vector-2"
+          src="https://anima-uploads.s3.amazonaws.com/projects/6402851d6a37db7167320ed4/releases/64028608c9953e08464983be/img/vector-1.svg"
+          alt="Vector"
+        />
+      </div>
+      <div class="unit-container">
+        <h1 class="title">{{ title }}</h1>
+        <div class="row-format-container">
+          <table-row :className="tableRow1Props.className" />
+          <table-row2 :className="tableRow2Props.className" />
+          <table-row3 :className="tableRow3Props.className" />
+          <table-row4 :className="tableRow4Props.className" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Ellipse6 from "./Ellipse6";
+import TableRow from "./TableRow";
+import TableRow2 from "./TableRow2";
+import TableRow3 from "./TableRow3";
+import TableRow4 from "./TableRow4";
+export default {
+  name: "WebsiteUnitsPage",
+  components: {
+    Ellipse6,
+    TableRow,
+    TableRow2,
+    TableRow3,
+    TableRow4,
+  },
+  props: [
+    "defaultLogo4",
+    "title",
+    "ellipse6Props",
+    "tableRow1Props",
+    "tableRow2Props",
+    "tableRow3Props",
+    "tableRow4Props",
+  ],
+};
+</script>
+
+<style>
+.website-units-page {
+  position: relative;
+  width: 100%; 
+  height: 100vh;
+  background: #FFFFFF;
+}
+
+.default-logo-4 {
+  position: absolute;
+  max-width: 100%;
+  width: 10vw;
+  height: auto;
+  left: 5.8333vw;
+  top: 3vh;
+  object-fit: cover;
+}
+
+.vector-2 {
+  position: absolute;
+  width: 4vw;
+  height: auto;
+  left: 93vw;
+  top: 3vh;
+  object-fit: cover;
+}
+
+.profile-img {
+  position: absolute;
+  width: 6vw;
+  height: auto;
+  left: 87vw; 
+  top: 3vh;
+  object-fit: cover;
+}
+
+.title {
+  position: absolute;
+  color: var(--black);
+  left: 6vw;
+  width: 25vw;
+  height: 3em;
+  line-height: 2.5vw;
+  text-align: center;
+  font-weight: 600;
+  font-size: 2vw;
+  font-family: 'Inter';
+  font-style: normal;
+}
+
+.unit-container {
+  position: absolute;
+  left: 35vw;
+  top: 10vh;
+  height: auto;
+}
+
+.row-format-container {
+  position: absolute;
+  top: 10vh;
+}
+</style>
