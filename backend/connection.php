ffee3939e27b71d87782a17c1d1026cee27fa166
Valerie Wong <wongvalerie45@gmail.com>
2023-03-07 01:42:05 -0500

    Database works locally #38

--- backend/connection.php
+++ backend/connection.php
@@ -1,9 +1,15 @@
 <?php 
 
-    $dbhost = "oceanus";
-    $dbuser = "vwong27";
-    $dbpass = "50342607";
-    $dbname = "cse442_2023_spring_team_a_db";
+    // $dbhost = "oceanus";
+    // $dbuser = "vwong27";
+    // $dbpass = "50342607";
+    // $dbname = "cse442_2023_spring_team_a_db";
+
+    //Local stuff
+    $dbhost = "localhost";
+    $dbuser = "root";
+    $dbpass = "";
+    $dbname = "vue_php_cdn_db";
 
     // Create connection
     $conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
--- backend/register.php
+++ backend/register.php
@@ -4,18 +4,35 @@
     session_start();
     include("connection.php");
     include("functions.php");
+
+    if (isset($_SERVER["HTTP_ORIGIN"])) {
+        // Decide if the origin in $_SERVER['HTTP_ORIGIN'] is the one that you want to allow
+        header("Access-Control-Allow-Origin: *");
+        header("Access-Control-Allow-Credentials: true");
+        header("Access-Control-Max-Age: 1000");
+    }
+
+    if ($_SERVER["REQUEST_METHOD"] == "OPTIONS") {
+        if (isset($_SERVER["HTTP_ACCESS_CONTROL_REQUEST_METHOD"])) {
+            header("Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE");
+        }
+        if (isset($_SERVER["HTTP_ACESS_CONTROL_REQUEST_HEADERS"])) {
+            header("Access-Control-Allow-Headers: Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization");
+        }
+        exit(0);
+    }
     
     if($_SERVER["REQUEST_METHOD"]  == "POST"){
 
-        $user_name = $_POST["user_name"];
+        $user_name = $_POST["username"];
         $email = $_POST["email"];
         $password = $_POST["password"];
 
         if(!empty($user_name) && !empty($password) && !is_numeric($user_name)){
             //Generate random userid, save username and email 
             $user_id = random_num(20);
-            $query = "insert into users (user_id,user_name,email) values ('$user_id','$user_name','$email')";
-            mysqli_query($con, $query);
+            $query = "INSERT INTO `test` (`user_id`, `user_name`, `email`) VALUES ('$user_id','$user_name','$email')";
+            $res = $conn -> query($query);
         } else {
             echo "Please enter some valid information!";
         }
