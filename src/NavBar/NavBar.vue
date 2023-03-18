1cc095feedb93465b99a952252f630e564d17d30
Cheri342 <71862946+Cheri342@users.noreply.github.com>
2023-03-18 14:43:00 -0400

    Added styling to profile loggedin #51

--- src/NavBar/NavBar.vue
+++ src/NavBar/NavBar.vue
@@ -20,8 +20,6 @@
 
 <script>
 import MenuBarLoggedIn from "@/components/menuBars/menuBarLoggedIn.vue";
-
-
 export default {
   name: "NavBar",
   components: {
@@ -32,7 +30,6 @@ export default {
     return {
       //Store variables in here
       navInfo: {
-
       }
     }
   },
@@ -59,7 +56,6 @@ export default {
   margin-top: 1.3vh;
   z-index: 1;
 }
-
 .profile_image_loggedin{
   position: absolute;
   margin: 0;
@@ -72,7 +68,6 @@ export default {
   overflow: visible;
   white-space: nowrap;
 }
-
 .main_logo {
   background-image: url("../../img/Logo.png");
   background-repeat: no-repeat;
@@ -86,10 +81,7 @@ export default {
   width: 15%;
   overflow: hidden;
   max-width: 100%;
-
-
 }
-
 .main_menu_container {
   position: absolute;
   margin: 0;
@@ -101,18 +93,13 @@ export default {
   max-width: 100%;
   overflow: visible;
   white-space: nowrap;
-
 }
-
 .menu-edit {
   top: 27%;
   left: 35%;
   height: 55px;
   width: 45px;
 }
-
-
-
 @media only screen and (min-width: 760px) and (max-width: 1300px) {
   .main_logo {
     scale: 1.4;
@@ -121,28 +108,36 @@ export default {
   .main_menu_container {
     left: 77.5%;
   }
-}
 
+  .profile_image_loggedin{
+    margin-top: -0.4%; 
+    height: 40px;
+    left: 70.5%; 
+  }
+}
 @media only screen and (min-width: 500px) and (max-width: 759px) {
   .main_logo {
     scale: 1.7;
     left: 5.1%;
   }
-
   .menu-edit {
     top: 1.9%;
     left: 80.5%;
     height: 55px;
     width: 45px;
   }
-}
 
+  .profile_image_loggedin{
+    margin-top: -1.8%; 
+    height: 40px; 
+    left: 64%; 
+  }
+}
 @media screen and (min-width: 200px) and (max-width: 499px) {
   .main_logo {
     scale: 2.1;
     left: 12.1%;
   }
-
   .menu-edit {
     top: 1.1%;
     left: 75.5%;
@@ -150,5 +145,11 @@ export default {
     width: 45px;
   }
 
+  .profile_image_loggedin{
+    margin-top: -2%; 
+    height: 40px; 
+    left: 60%; 
+    transform: scale(0.8)
+  }
 }
 </style>
