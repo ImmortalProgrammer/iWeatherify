881acaeb61290b54480731b64c00a0fc18657492
shlc001 <98677355+shlc001@users.noreply.github.com>
2023-03-18 22:19:09 -0400

    Fixed NavBar -> Finishing Touches #51

--- src/NavBar/NavBar.vue
+++ src/NavBar/NavBar.vue
@@ -4,26 +4,32 @@
     <div class="main_logo"></div>
 
     <div class="main_menu_container">
-        <menu-bar-logged-in class = "menu-edit"></menu-bar-logged-in>
+      <menu-bar-logged-in class = "menu-edit"></menu-bar-logged-in>
     </div>
-      <!-- Implement Profile html here
-      <img class ="profileLocation" src="../../img/Profile image.png" @click="onProfileClick()" />
-      !-->
+
+    <div class = "profile_image_loggedin">
+      <a id ="savedoutfits" :href="$router.resolve('/savedOutfits').href" style = "text-decoration:none; color: inherit;">
+        <img src="../../img/Profile image.png" alt="ProfileImage"/> </a>
+    </div>
+
 
   </div>
+
+
 </template>
 
 <script>
 import MenuBarLoggedIn from "@/components/menuBars/menuBarLoggedIn.vue";
-
 export default {
   name: "NavBar",
-  components: {MenuBarLoggedIn},
+  components: {
+    MenuBarLoggedIn,
+
+  },
   data() {
     return {
       //Store variables in here
       navInfo: {
-
       }
     }
   },
@@ -50,7 +56,17 @@ export default {
   margin-top: 1.3vh;
   z-index: 1;
 }
-
+.profile_image_loggedin{
+  position: absolute;
+  margin: 0;
+  left: 79%;
+  object-fit: scale-down;
+  height: 100%;
+  width: 7%;
+  max-width: 100%;
+  overflow: visible;
+  white-space: nowrap;
+}
 .main_logo {
   background-image: url("../../img/Logo.png");
   background-repeat: no-repeat;
@@ -64,10 +80,7 @@ export default {
   width: 15%;
   overflow: hidden;
   max-width: 100%;
-
-
 }
-
 .main_menu_container {
   position: absolute;
   margin: 0;
@@ -79,16 +92,13 @@ export default {
   max-width: 100%;
   overflow: visible;
   white-space: nowrap;
-
 }
-
 .menu-edit {
   top: 27%;
   left: 35%;
   height: 55px;
   width: 45px;
 }
-
 @media only screen and (min-width: 760px) and (max-width: 1300px) {
   .main_logo {
     scale: 1.4;
@@ -97,28 +107,36 @@ export default {
   .main_menu_container {
     left: 77.5%;
   }
-}
 
+  .profile_image_loggedin{
+    margin-top: -0.4%;
+    height: 40px;
+    left: 70.5%;
+  }
+}
 @media only screen and (min-width: 500px) and (max-width: 759px) {
   .main_logo {
     scale: 1.7;
     left: 5.1%;
   }
-
   .menu-edit {
     top: 1.9%;
     left: 80.5%;
     height: 55px;
     width: 45px;
   }
-}
 
+  .profile_image_loggedin{
+    margin-top: -1.8%;
+    height: 40px;
+    left: 64%;
+  }
+}
 @media screen and (min-width: 200px) and (max-width: 499px) {
   .main_logo {
     scale: 2.1;
     left: 12.1%;
   }
-
   .menu-edit {
     top: 1.1%;
     left: 75.5%;
@@ -126,5 +144,11 @@ export default {
     width: 45px;
   }
 
+  .profile_image_loggedin{
+    margin-top: -2%;
+    height: 40px;
+    left: 60%;
+    transform: scale(0.8)
+  }
 }
 </style>
