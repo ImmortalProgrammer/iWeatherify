8e6c0d98b2d955a98d8bc99754fb1b665ca6f13a
Valerie Wong <wongvalerie45@gmail.com>
2023-04-05 19:12:48 -0400

    Bringing back old savedOutfits component and old savedOutfits route info #63

--- src/router.js
+++ src/router.js
@@ -358,8 +358,11 @@ const routes = [
     path: "/savedOutfits",
     component: WebsiteProfileNoSavedOutfit,
     props: {
+      plusMath:
+          "https://anima-uploads.s3.amazonaws.com/projects/6405ee9a4548b3806a50e138/releases/6405ef8c246cb8c2cd3b302a/img/plus-math@2x.png",
       defaultFrameLogo3:
           "https://anima-uploads.s3.amazonaws.com/projects/6405ee9a4548b3806a50e138/releases/6405ef8c246cb8c2cd3b302a/img/default-frame-logo-2@2x.png",
+      noSavedOutfitsTrySavingOne: "No saved outfits, try saving one!",
       ellipse6Props: websiteProfileNoSavedOutfitData.ellipse6Props,
     },
     meta: {
