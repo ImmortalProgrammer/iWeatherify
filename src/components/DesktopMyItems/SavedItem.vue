e5333864be0bfde04042e908e528bf88a3c69eb1
Valerie Wong <wongvalerie45@gmail.com>
2023-04-12 10:39:04 -0400

    Start of displaying images as a SavedItem component #80

---
+++ src/components/DesktopMyItems/SavedItem.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="container">
+        <div class="name-container">
+            <h1>{{ image_name }}</h1>
+        </div>
+
+        <div class="image-container">
+            <img :src="require(`@/assets/${upload_path}`)">
+        </div>
+    </div>
+</template>
+
+<script> 
+export default {
+    props: ["image_name", "upload_path"],
+    components: {
+
+    },
+    data(){
+
+    },
+    methods: {
+        getImageUrl(){
+            return this.upload_path
+        }
+    }
+    
+}
+</script>
+
+<style scoped>
+
+.container{
+    width: 30vw;
+    height: 40vh;
+    background: blue;
+}
+
+</style>
--- src/components/DesktopMyItems/TempItem.vue
+++ src/components/DesktopMyItems/TempItem.vue
@@ -3,6 +3,16 @@
         <!-- Hidden modal -->
         <modal v-show="isModalVisible" @close="closeModal" :title="temp_and_clothing" :temp_category="temp_category" :clothing_category="clothing_category"></modal>
 
+        <div class="All-my-items">
+          <saved-item 
+            v-for="item in items"
+            :key = "item.index"
+            :image_name = "item.image_name"
+            :upload_path = "item.upload_path"
+          >
+          </saved-item>
+        </div>
+
         <div class="All-my-clothes">
             <div class="plusButton" @click.self="showModal">
                 <img class="plus-math" src="../../../img/plus-math@2x.png" @click.self="showModal" alt="Plus Math" />  
@@ -12,19 +22,26 @@
 </template>
 
 <script>
+import axios from "axios"
 import NavBar from '../../NavBar/NavBar.vue';
 import Modal from "../Modal/Modal.vue";
+import SavedItem from "./SavedItem.vue";
 export default {
     name: "TempItem",
     components: {
         NavBar,
-        Modal
+        Modal,
+        SavedItem
     },
     props: [
 
     ],
     data(){
       return {
+        items: [
+          {image_name: "my jacket", upload_path: "bunny.jpg", index: 1},
+          {image_name: "my duck", upload_path: "duck.jpg", index: 2}
+        ],
         isModalVisible: false,
         temp_and_clothing: this.$route.name,
         temp_category: this.$route.meta.temp_category,
