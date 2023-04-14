5fae7f70fd0c84d3fd56c9732840c43a970c51ae
Valerie Wong <wongvalerie45@gmail.com>
2023-04-14 13:11:28 -0400

    Able to parse through database and send list of objects to frontend. Need frontend to process blob #80

--- src/components/DesktopMyItems/TempItem.vue
+++ src/components/DesktopMyItems/TempItem.vue
@@ -6,8 +6,8 @@
         <div class="All-my-items">
           <saved-item class="savedItem"
             v-for="item in items"
-            :key = "item.index"
-            :image_name = "item.image_name"
+            :key = "item.id"
+            :image_name = "item.clothing_name"
             :upload_path = "item.upload_path"
           >
           </saved-item>
@@ -38,18 +38,23 @@ export default {
     ],
     data(){
       return {
-        items: [
-          {image_name: "my jacket", upload_path: "bunny.jpg", index: 1},
-          {image_name: "my duck", upload_path: "duck.jpg", index: 2},
-          {image_name: "cute", upload_path: "cute_bunny.jpg", index: 1},
-          {image_name: "cuteeeeeeeeeeeee", upload_path: "cute_bunny_2.jpg", index: 2},
-          {image_name: "Actual jacket", upload_path: "Summer jacket.png", index: 1},
-        ],
+        // items: [
+        //   {image_name: "my jacket", upload_path: "bunny.jpg", index: 1},
+        //   {image_name: "my duck", upload_path: "duck.jpg", index: 2},
+        //   {image_name: "cute", upload_path: "cute_bunny.jpg", index: 1},
+        //   {image_name: "cuteeeeeeeeeeeee", upload_path: "cute_bunny_2.jpg", index: 2},
+        //   {image_name: "Actual jacket", upload_path: "Summer jacket.png", index: 1},
+        // ],
+        items: null,
         isModalVisible: false,
+        userid: null,
         temp_and_clothing: this.$route.name,
         temp_category: this.$route.meta.temp_category,
-        clothing_category: this.$route.meta.clothing_category
+        clothing_category: this.$route.meta.clothing_category,
       }
+    },
+    created() {
+      this.getUserId().then(() => {this.getAllItems()});
     },
     methods: {
       showModal(){
@@ -59,6 +64,37 @@ export default {
       closeModal(){
         this.isModalVisible = false
         console.log("Closing the modal")
+      },
+      async getUserId() {
+        try {
+          const response = await axios.get("http://localhost/project_s23-iweatherify/backend/get_userid.php", { withCredentials: true });
+          this.userid = response.data.userid;
+          console.log("The user id is: " + this.userid)
+        } catch (error) {
+          console.error("Unsuccessful request in getUserId().", error);
+        }
+      },
+      getAllItems(){
+        console.log(this.userid)
+        console.log(this.temp_category)
+        console.log(this.clothing_category)
+        axios.post("http://localhost/project_s23-iweatherify/backend/get_my_items.php", 
+        {
+          user_id: this.userid,
+          temp_category: this.temp_category,
+          clothing_category: this.clothing_category
+        })
+        .then(res => {
+          // if(res != null){
+          //   this.items = res.data.message  
+          // }
+          // console.log(res.data.message[0].clothing_name)
+          let lod = JSON.parse(JSON.stringify(res.data.message))
+          console.log(lod)
+          this.items = lod
+        }).catch((err) => {
+          console.log(err)
+        })
       }
     },
 }
