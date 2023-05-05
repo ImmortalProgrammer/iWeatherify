a902c26cd1701f9407f1555ac4a7b8856057e78b
JayXPan <jpan26@buffalo.edu>
2023-05-05 19:05:02 -0400

    Testing server environmental variables 2nd try #108

--- backend/connection.php
+++ backend/connection.php
@@ -1,8 +1,15 @@
 <?php 
-    $dbhost = "oceanus";
-    $dbuser = "vwong27";
-    $dbpass = "50342607";
-    $dbname = "cse442_2023_spring_team_a_db";
+
+    // $dbhost = "oceanus";
+    // $dbuser = "vwong27";
+    // $dbpass = "50342607";
+    // $dbname = "cse442_2023_spring_team_a_db";
+    require_once 'loadenv.php';
+
+    $dbhost = getenv('DB_HOST');
+    $dbuser = getenv('DB_USER');
+    $dbpass = getenv('DB_PASS');
+    $dbname = getenv('DB_NAME');
     
     // Create connection
     $conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
